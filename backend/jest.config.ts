2d9a98f864b4eded235606ac16dc6b8ff1a67aeb
Mostafa <mostafa@elmostafa.com>
2026-07-03 19:46:17 +0300

    feat: add jest.config.ts with coverage thresholds and remove inline jest config

---
+++ backend/jest.config.ts
@@ -0,0 +1,19 @@
+export default {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+  coverageThreshold: {
+    global: {
+      branches: 60,
+      functions: 60,
+      lines: 60,
+      statements: 60,
+    },
+  },
+};
